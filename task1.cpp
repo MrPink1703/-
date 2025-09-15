647b8e4dfa7d5c361a9397c0ab41cc6af5a15eac
MrPink1703 <mal_art07@mail.ru>
2025-09-15 16:29:21 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,15 @@
+list<int> this_list = {0, 2, 4, 6, 8} //Представление списка на C++
+
+
+#include <iostream> //Представление организации стека на С++
+#include <stack>
+ 
+int main()
+{
+    std::stack<std::string> stack;
+
+    stack.push("Tom");
+    stack.push("Bob");
+    stack.push("Sam");
+
+}
---
+++ стек.java
@@ -0,0 +1,6 @@
+// Создаем стек
+Stack<Integer> stack = new Stack<>();
+
+// Заполняем стек числами от 1 до 10
+for (int i = 1; i <= 10; i++) {
+       stack.push(i)
